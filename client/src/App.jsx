ee33269843e0086460bee31152c5ebac0319f7ac
saikirandamerla <154958470+saik2422@users.noreply.github.com>
2025-04-27 11:11:21 +0530

    files added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './assets/components/home';
+import AddWorkerPage from './assets/components/addworker';
+import Navbar from './assets/components/navbar';
+/*import Details from './assets/components/details';
+import Dashboard from './assets/components/data';
+import GetDetails from './assets/components/getdetails'
+import Safety from './assets/components/safefy';
+import Appointment from './assets/components/appointments';
+import CalendarPage from './assets/components/calender';
+import Emergency from './assets/components/emergency';
+import Details from './assets/components/details';*/
+
+import HomeWork from "../../server/components/homework";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const router = createBrowserRouter([
+   {
+      path: '/HomeWork',
+      element: (
+        <>
+          <HomeWork/>
+        </>
+      ),
+    },
+    {
+      path: '/',
+      element: (
+        <>
+        
+          <HomeWork/>
+        </>
+      ),
+    }
+
+/*{
+      path: '/Dashboard',
+      element: (
+        <>
+          <Navbar />
+          <Dashboard/>
+        </>
+      ),
+    }*/
+  ])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <RouterProvider router={router}/>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
 
-export default App
+export default App;
